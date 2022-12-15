99e62070914158bf346c01b7aad85be035709d41
markgolubev <markflyingbear@gmail.com>
2022-12-15 11:19:24 +0000

    added: additional check for word count and start word, start game button with functional, refactored: alert notice. fixed bug when have the ability to enter word twice using lowercase and uppercase

--- Word Scrumble/TableViewController.swift
+++ Word Scrumble/TableViewController.swift
@@ -11,6 +11,13 @@ class TableViewController: UITableViewController {
     var allWords = [String]()
     var usedWords = [String]()
     
+    enum errorType {
+        case notPossible
+        case notOriginal
+        case notReal
+        case startWord
+    }
+    
     
 
     override func viewDidLoad() {
@@ -18,6 +25,8 @@ class TableViewController: UITableViewController {
         
         // added add button with closure (see below)
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promtForAnswer))
+        // added new game button
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "New game", style: .plain, target: self, action: #selector(startGame))
         
         // find file
         if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
@@ -34,9 +43,10 @@ class TableViewController: UITableViewController {
         startGame()
     }
     
-    func startGame() {
+    @objc func startGame() {
         title = allWords.randomElement()
         usedWords.removeAll(keepingCapacity: true)
+        // update table data
         tableView.reloadData()
     }
     
@@ -79,40 +89,35 @@ class TableViewController: UITableViewController {
     func submit(_ answer: String) {
         let lowerAnswer = answer.lowercased()
         
-        let errorTitle: String
-        let errorMessage: String
-        
         if isPossible(word: lowerAnswer) {
             if isOriginal(word: lowerAnswer) {
                 if isReal(word: lowerAnswer) {
-                    usedWords.insert(answer, at: 0)
-                    
-                    let indexPath = IndexPath(row: 0, section: 0)
-                    tableView.insertRows(at: [indexPath], with: .automatic)
-                    return
+                    if isStartWord(word: lowerAnswer){
+                        usedWords.insert(lowerAnswer, at: 0)
+                        
+                    // update one row
+                        let indexPath = IndexPath(row: 0, section: 0)
+                        tableView.insertRows(at: [indexPath], with: .automatic)
+                        return
+                    } else {
+                        showErrorMessage(error: .startWord)
+                    }
                 } else {
-                    errorTitle = "Word not recognized"
-                    errorMessage = "You can't just make them up, you know!"
+                    showErrorMessage(error: .notReal)
                 }
             } else {
-                errorTitle = "Word already used"
-                errorMessage = "Be more original!"
+                showErrorMessage(error: .notOriginal)
             }
         } else {
-            guard let title = title else { return }
-            errorTitle = "Word not possible"
-            errorMessage = "You can't spell that word from \(title.lowercased())."
+            showErrorMessage(error: .notPossible)
         }
-        let ac = UIAlertController(title: errorTitle, message: errorMessage, preferredStyle: .alert)
-        ac.addAction(UIAlertAction(title: "Ok", style: .default))
-        
-        present(ac, animated: true)
     }
     
     func isPossible(word: String) -> Bool {
         guard var tempWord = title?.lowercased() else { return false }
         
         for letter in word {
+            
             if let position = tempWord.firstIndex(of: letter) {
                 tempWord.remove(at: position)
             } else {
@@ -131,7 +136,37 @@ class TableViewController: UITableViewController {
         let checker = UITextChecker()
         let range = NSRange(location: 0, length: word.utf16.count)
         let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
-        return misspelledRange.location == NSNotFound
+        return misspelledRange.location == NSNotFound && word.utf16.count > 2
+    }
+    
+    func isStartWord(word: String) -> Bool {
+        return word != title
+    }
+    
+    func showErrorMessage(error: errorType) {
+        let errorTitle: String
+        let errorMessage: String
+        
+        switch error {
+        case .notPossible:
+            guard let title = title else { return }
+            errorTitle = "Word not possible"
+            errorMessage = "You can't spell that word from \(title.lowercased())."
+        case .notOriginal:
+            errorTitle = "Word already used"
+            errorMessage = "Be more original!"
+        case .notReal:
+            errorTitle = "Word not recognized"
+            errorMessage = "You can't just make them up, you know!"
+        case .startWord:
+            errorTitle = "Word is equal to start word"
+            errorMessage = "You can't use start word!"
+        }
+        
+        let ac = UIAlertController(title: errorTitle, message: errorMessage, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Ok", style: .default))
+        
+        present(ac, animated: true)
     }
 
 
